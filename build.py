a1d0e9c3886fc9c87bae0eb1400767145e1876ba
dylansdaniels <dylansdaniels@gmail.com>
2025-06-10 17:48:20 -0400

    add complete button formatting

--- build.py
+++ build.py
@@ -245,9 +245,19 @@ def generate_page_html(page_paths):
             nb_arguments_pattern = ".ipynb]["
 
             nb_button="""
-        <div>
-            <a href='notebook_name'>
-                <button class='notebook-download'></button>
+        <div class="notebook-download-wrapper">
+            <a href='notebook_name' download>
+                <button class="notebook-download">
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        width="20" height="20" viewBox="0 0 24 24" fill="none" 
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+                        <polyline points="7 10 12 15 17 10"/>
+                        <line x1="12" y1="15" x2="12" y2="3"/>
+                    </svg>
+                    <span style="width: 8px"></span>
+                    <span>Download Notebook</span>
+                </button>
             </a>
         </div>
 
